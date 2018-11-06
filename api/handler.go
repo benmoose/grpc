0a2e7075a37ca272bbd78e2b440ff551cdc3a7d8
Ben Hadfield <benjohnhadfield@gmail.com>
2018-11-06 02:09:15 +0000

    add server (simple)

--- api/handler.go
+++ api/handler.go
@@ -8,7 +8,7 @@ import (
 type Server struct {
 }
 
-func (s *Server) SayHello (ctx context.Context, msg PingMessage) (*PingMessage, error) {
+func (s *Server) SayHello (ctx context.Context, msg *PingMessage) (*PingMessage, error) {
 	log.Printf("Received message: %s", msg.Message)
 	return &PingMessage{Message: "Hello from Server!"}, nil
 }
---
+++ server/main.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"fmt"
+	"github.com/benjaminhadfield/grpc/api"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := api.Server{}
+	grpcServer := grpc.NewServer()
+	api.RegisterPingServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
